c4d127bee0dafd753138376901f4482cb7958648
Alexander Fitterling <info@sp33c.de>
2014-01-17 15:08:34 +0100

    Revert "sign up mailer"
    This reverts commit af9f79c73844a28aa840368c4b6393ff72c090d9.

--- app/mailers/sign_up.rb
+++
@@ -1,3 +0,0 @@
-class SignUp < ActionMailer::Base
-  default from: "from@example.com"
-end
--- test/mailers/sign_up_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SignUpTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
