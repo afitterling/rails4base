af9f79c73844a28aa840368c4b6393ff72c090d9
Alexander Fitterling <info@sp33c.de>
2014-01-17 14:50:44 +0100

    sign up mailer

---
+++ app/mailers/sign_up.rb
@@ -0,0 +1,3 @@
+class SignUp < ActionMailer::Base
+  default from: "from@example.com"
+end
---
+++ test/mailers/sign_up_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SignUpTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
