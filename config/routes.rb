0f959b9fb573f263c6944bd4158bddaab9997ecf
Alexander Fitterling <info@sp33c.de>
2014-01-18 11:06:56 +0100

    routing server side to angular side

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,20 @@ App::Application.routes.draw do
 
   devise_for :users, skip: :all
   devise_scope :user do
-    post "users/sign_in" => "users/sessions#create"
-    get "users/logout" => "users/sessions#destroy"
-    post "users/sign_up" => "users/registrations#create"
-    get "users/restore" => "users/sessions#show_current_user"
+    post 'users/sign_in' => 'users/sessions#create'
+    get 'users/logout' => 'users/sessions#destroy'
+    post 'users/sign_up' => 'users/registrations#create'
+    get 'users/restore' => 'users/sessions#show_current_user'
   end
 
-  # route to angular app
-  get "/" => "pages#index"
-  get "/pages/" => "pages#index"
+  # Client:toChooseUrl --req--> Rack --routes--> Controller:pages#index --renders Layout:withoutYield --> Client:withChosenUrl
+  # --> angular:picksBrowserUrl --pushState-> Client
 
-  # angular template controller for partials/directives (see directives)
-  get "/angular/:template_class/:template_name" => "angular_templates#show"
+  # route any client reqs to angular main app
+  get '/' => 'pages#index'
+  get '/(:pages(/:subpage))' => 'pages#index'
+
+  # angular template controller for partials/directives used in angular code parts
+  get '/angular/:template_class/:template_name' => 'angular_templates#show'
 
 end
