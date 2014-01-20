d7ca7f2a230350b2837039c287eb46a6b3af4022
Alexander Fitterling <info@sp33c.de>
2014-01-20 14:10:26 +0100

    devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,13 +2,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   respond_to :json
 
-  # I extended from basics: http://natashatherobot.com/devise-sign-up-ajax-rails/
-
   def create
     build_resource
 
     resource = User.create(filtered_params)
-    #resource.genPassword # auto gen password
+
     password = ('0'..'z').to_a.shuffle.first(8).join
     resource.password = password
 
@@ -20,31 +18,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
       # send 406 - resource not acceptable due to validation issues
       render json: { success: false, errors: resource.errors.full_messages }, status: 406
     end
-
-    begin
-      #return render json: { status: 200, success: true, user: resource}
-      #begin
-      #  sign_up(resource_name, resource)
-      #rescue
-      #  return render json: { status: 401, success: false}
-      #end
-    #rescue
-    #  return render json: { status: 500, success: false, info: resource.errors.full_messages}
-    end
-
-    #if resource.save
-    #  if resource.active_for_authentication?
-    #    sign_up(resource_name, resource)
-    #    return render json: { status: 200, success: true, user: resource}
-    #  else
-    #    expire_session_data_after_sign_in!
-    #    return render json: { status: 200, success: true, user: resource}
-    #  end
-    #else
-    #  clean_up_passwords resource
-    #  # @TODO correct error code
-    #  return render json: { status: 401, success: false}
-    #end
 
   end
 
