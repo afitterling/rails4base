67a4b3d583ac3dc548491a8d1da950e24eb39977
Alexander Fitterling <info@sp33c.de>
2014-01-23 11:16:28 +0100

    Revert "missing response detection"
    This reverts commit 260121d3fab5a1d5a9be494f9405bce7adfb3d9e.

--- app/assets/javascripts/scripts/app.js
+++ app/assets/javascripts/scripts/app.js
@@ -87,9 +87,8 @@ app.config(
 
 // init some stuff right after bootstraping angular
 
-app.run(['$rootScope', '$http', 'logService', 'Session', '$location', '$templateCache','$timeout', function ($rootScope, $http, logService, Session, $location, $templateCache, $timeout) {
+app.run(['$rootScope', '$http', 'logService', 'Session', '$location', '$templateCache', function ($rootScope, $http, logService, Session, $location, $templateCache) {
 
-  var crashWarningTimeOut = 5000;
 
   // pre-cache important templates
   // @TODO get templates per json from rails
@@ -148,15 +147,7 @@ app.run(['$rootScope', '$http', 'logService', 'Session', '$location', '$template
   });
 
   $rootScope.$on('$routeChangeStart', function (e, next, cur) {
-    $rootScope.templateLoaded = false
     $rootScope.loadingTemplate = true
-    $timeout(function () {
-      if ($rootScope.templateLoaded == false) {
-        // this should not happen in production
-        // @TODO check if requests are active &&
-        $rootScope.crashed = true
-      }
-    }, crashWarningTimeOut)
   });
 
   $rootScope.$on('$routeChangeSuccess', function (e, cur, prev) {
@@ -164,7 +155,6 @@ app.run(['$rootScope', '$http', 'logService', 'Session', '$location', '$template
   });
 
   $rootScope.$on('$viewContentLoaded', function () {
-    $rootScope.templateLoaded = true
     $rootScope.loadingTemplate = false
   });
 
