ffab6cccf3d6ec4d683392c90c65ae3459957028
Alexander Fitterling <info@sp33c.de>
2014-01-23 20:13:54 +0100

    restrict json api

--- app/controllers/api/demo_controller.rb
+++ app/controllers/api/demo_controller.rb
@@ -1,8 +1,16 @@
 class Api::DemoController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :restrict
 
   def index
     render json: { text: "Just a Demonstration!" }
   end
+
+  private
+
+    def restrict
+      unless user_signed_in?
+        render json: {}, status: 401
+      end
+    end
 
 end
