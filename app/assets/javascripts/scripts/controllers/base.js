18f48cfc0b7be490d4c2280db2a105060bcb069c
Alexander Fitterling <info@sp33c.de>
2014-01-23 23:02:50 +0100

    works

---
+++ app/assets/javascripts/scripts/controllers/base.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ctrls.base', [])
+
+  .controller('BaseCtrl',
+    ['$scope', '$rootScope', '$resource', '$http','$location',
+      function ($scope, $rootScope, $resource, $http, $location) {
+
+      }]);
+
