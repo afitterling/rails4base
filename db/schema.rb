e12fd18859b5a05c5206081362c073483d914df7
Alexander Fitterling <info@sp33c.de>
2014-01-24 23:23:36 +0100

    Revert "confirmation  mail"
    This reverts commit d004de35449b77f537cafd47a86039f808d81b02.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124211856) do
+ActiveRecord::Schema.define(version: 20131227074029) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,12 +26,8 @@ ActiveRecord::Schema.define(version: 20140124211856) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
