f1a7815cf0cf254b0115609978ff38aaba1da0fb
Alexander Fitterling <info@sp33c.de>
2014-01-27 14:04:24 +0100

    mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ App::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => "rails4base.sp33c.de" }
+  config.action_mailer.default_url_options = { :host => APP_CONFIG[:mail][:domain] }
 
   config.action_mailer.smtp_settings = {
   #    address: "mail.seamlesslywired.com",
